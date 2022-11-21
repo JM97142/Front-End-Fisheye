46d55272b1fd5ab4300876d214cfebf37aed0d70
JM97142 <jeanmichel.taret@gmail.com>
2022-11-21 06:21:23 +0100

    Part.2 Importer les datas

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,17 +1,33 @@
 function photographerFactory(data) {
-    const { name, portrait } = data;
 
+    const { portrait, name, city, country, tagline, price, id } = data;
     const picture = `assets/photographers/${portrait}`;
 
     function getUserCardDOM() {
         const article = document.createElement( 'article' );
+
         const img = document.createElement( 'img' );
         img.setAttribute("src", picture)
+
         const h2 = document.createElement( 'h2' );
         h2.textContent = name;
+
+        const h3 = document.createElement( 'h3' );
+        h3.textContent = city + country;
+
+        const h4 = document.createElement( 'h4' );
+        h4.textContent = tagline;
+
+        const p = document.createElement( 'p' );
+        p.textContent = price;
+
         article.appendChild(img);
         article.appendChild(h2);
+        article.appendChild(h3);
+        article.appendChild(h4);
+        article.appendChild(p);
+
         return (article);
     }
-    return { name, picture, getUserCardDOM }
+    return { picture, name, city, country, tagline, price, id, getUserCardDOM }
 }
